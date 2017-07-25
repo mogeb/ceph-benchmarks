b17cdf80e6e19c2fcf2879c884af1622b121c92e
Mohamad Gebai <mgebai@suse.com>
2017-07-25 13:52:51 +0300

    Add average and stddev functions to common.bash
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- common/common.bash
+++ common/common.bash
@@ -25,3 +25,27 @@ function log_configuration {
 
     cp -ar /srv/pillar/ceph/proposals/ results/
 }
+
+function average {
+    declare -a table=("${!1}")
+    local size=${#table[*]}
+    local result=0
+    for v in ${table[@]}; do
+    result=`echo "$result + $v" | bc -l`
+    done
+    result=`echo "$result / $size" | bc -l`
+    printf "%.0f" $result
+}
+
+function stdev {
+    declare -a table=("${!1}")
+    local size=${#table[*]}
+    local sum=0
+    local sumsq=0
+    for v in ${table[@]}; do
+    sum=`echo "$sum + $v" | bc -l`
+    sumsq=`echo "$sumsq + $v*$v" | bc -l`
+    done
+    local result=`echo "sqrt($sumsq/$size - ($sum/$size)^2)" | bc -l`
+    printf "%.0f" $result
+}

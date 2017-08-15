e3e02ae58ded15a49cb23066698a433f6303c0e8
Mohamad Gebai <mgebai@suse.com>
2017-08-15 14:43:23 +0300

    Add scripts for comparing performance
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

---
+++ comparing-performance/do_bench.bash
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+source ../common/common.bash
+
+POOL=benchpool
+IMAGE=image01
+
+# log Cluster details
+log_configuration
+
+osds=("host1" "host2")
+
+# runs (for volatility)
+for RUN in 1 2; do
+    # patterns
+    for WORKLOAD in randread randwrite randrw; do
+        # enabled features
+        for BLOCKSIZE in 4k 64k 1m 2m; do
+            # number of jobs
+            for NJOBS in 1 8 16; do
+                DIR=results/run$RUN/$WORKLOAD/$BLOCKSIZE
+                RES=$DIR/summary_j${NJOBS}.csv
+                if [ -f $RES ] ; then
+                    echo
+                    echo "$RES exists, skipping.."
+                    echo
+                    echo
+                    continue
+                fi
+                mkdir -p $DIR
+
+                ceph osd pool create $POOL 12 12
+                rbd create $IMAGE --size 2048 --pool $POOL
+
+                if [ "$WORKLOAD" == "randread" ]; then
+                    rbd bench-write -p $POOL $IMAGE --io-size 4096 --io-threads 1 --io-total 64M --io-pattern rand
+                fi
+                cat bench.fio.template |
+                    sed "s/#POOL#/$POOL/g" |
+                    sed "s/#IMAGE#/$IMAGE/g" |
+                    sed "s/#NUM_JOBS#/$NJOBS/g" |
+                    sed "s/#WORKLOAD#/$WORKLOAD/g" |
+                    sed "s/#BLOCKSIZE#/$BLOCKSIZE/g" |
+                    sed "s!#LOGDIR#!$DIR!g"  > /tmp/bench.fio
+
+                cp /tmp/bench.fio $DIR
+                echo
+                echo "Starting collectl"
+                start_collectl run$RUN_$WORKLOAD_$BLOCKSIZE
+                echo
+                echo "Starting fio: Run $RUN, WL = $WORKLOAD, BS = $BLOCKSIZE, JOBS = $NJOBS"
+                fio --output-format=terse /tmp/bench.fio > $RES
+                echo "Done"
+                echo
+                echo "Stopping collectl"
+                stop_collectl
+                echo
+
+                for osd in "${osds[@]}"; do
+                    scp $osd:/tmp/ceph-benchmarks-run/* $DIR
+                done
+                salt '*' cmd.run 'rm -rf /tmp/ceph-benchmarks-run/'
+
+                # cleanup
+                rbd rm $POOL/$IMAGE
+            done
+        done
+    done
+done
+
+tar -czvf results.tar.gz results
---
+++ comparing-performance/process.bash
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+export LC_NUMERIC="en_US.UTF-8"
+
+source common.bash
+
+for folder in ses4-sp2-results ses5-sp3-results; do
+    printf "******************************\n"
+    printf "********* $folder ************\n"
+    #for w in randread; do
+    for WORKLOAD in randread randwrite randrw; do
+        printf "*********** $WORKLOAD workload *************\n"
+        printf "\t%8s \t\t%8s \t\t%8s \t\t%8s\n" "4k" "64k" "1m" "2m"
+
+        # FIRST_LEG="IOPS"
+        # SECOND_LEG="BW"
+        # FIRST_LEG="READ_CLAT"
+        # SECOND_LEG="WRITE_CLAT"
+        FIRST_LEG="READ_SLAT"
+        SECOND_LEG="WRITE_SLAT"
+        printf "\t%10s / %10s\t%10s / %10s\t%10s / %10s\t%10s / %10s\n" $FIRST_LEG $SECOND_LEG $FIRST_LEG $SECOND_LEG $FIRST_LEG $SECOND_LEG $FIRST_LEG $SECOND_LEG
+        for NJOBS in 1 8 16; do
+            printf "$NJOBS"
+            for BLOCKSIZE in 4k 64k 1m 2m; do
+                IOPS_SET=()
+                BW_SET=()
+                RCLAT_AVG_SET=()
+                WCLAT_AVG_SET=()
+                for RUN in 1 2; do
+                    DIR=$folder/run$RUN/$WORKLOAD/$BLOCKSIZE
+                    if [ -d "$DIR/j$NJOBS" ] ; then
+                        DIR=$DIR/j$NJOBS
+                    fi
+                    READ_IOPS=`awk -F ";" '{print $8}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    WRITE_IOPS=`awk -F ";" '{print $49}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    IOPS=$((READ_IOPS + WRITE_IOPS))
+                    IOPS_SET+=(${IOPS})
+
+                    READ_BW=`awk -F ";" '{print $7}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    WRITE_BW=`awk -F ";" '{print $48}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    BW=$((READ_BW + WRITE_BW))
+                    BW_SET+=(${BW})
+
+                    RCLAT_AVG=`awk -F ";" '{print $12}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    RCLAT_AVG=$((${RCLAT_AVG}/${NJOBS}))
+                    RCLAT_AVG_SET+=(${RCLAT_AVG})
+
+                    WCLAT_AVG=`awk -F ";" '{print $53}' $DIR/summary_j${NJOBS}.csv | awk 'BEGIN {sum=0;} {sum += $1} END { printf("%d", sum); }'`
+                    WCLAT_AVG=$((${WCLAT_AVG}/${NJOBS}))
+                    WCLAT_AVG_SET+=(${WCLAT_AVG})
+                done
+                AVG=`average IOPS_SET[@]`
+                STDEV=`stdev IOPS_SET[@]`
+                AVG_BW=`average BW_SET[@]`
+                AVG_RCLAT_AVG=`average RCLAT_AVG_SET[@]`
+                AVG_WCLAT_AVG=`average WCLAT_AVG_SET[@]`
+                # printf "\t%8s / %8s" $AVG $AVG_BW
+                printf "\t%10s / %10s" $AVG_RCLAT_AVG $AVG_WCLAT_AVG
+            done
+            printf "\n"
+        done
+        printf "\n"
+    done
+done
